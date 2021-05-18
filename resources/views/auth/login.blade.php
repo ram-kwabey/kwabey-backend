dbf118a2f922999b43bf0c44272ae7ff55b1b461
ram <rohit@kwabey.com>
2021-05-18 12:12:08 +0530

    add login and forgot form and css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,89 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center hold-transition">
-        <div class="col-md-4">
-        <div class="login-box">
-            <!-- /.login-logo -->
-            <div class="login-logo">
-                <a href="javascript:void(0)" style="color:#495057; text-decoration: none;"><b>{{ config('app.name', 'Kwabey') }}</a>
-            </div>
-            <div class="card">
-                <div class="card-body login-card-body">
-                <p class="login-box-msg" style="margin: 0;padding: 0 20px 20px;text-align: center;font-size:1rem;">
-                    Sign in to start your session</p>
-                <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                    <div class="input-group mb-3">
-                    <input id="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                        <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                    </div>
-                    <div class="input-group mb-3">
-                    <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                        <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                    </div>
-                    <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                        <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                        <label for="remember" style="font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">
-                            Remember Me
-                        </label>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                    </div>
-                    <!-- /.col -->
-                    </div>
-                </form>
-
-                <!-- /.social-auth-links -->
-
-                @if (Route::has('password.request'))
-                <p class="mb-1 mt-4">
-                    <a class="text-center" href="{{ route('password.request') }}">
-                        {{ __('Forgot Your Password?') }}
-                    </a>
-                </p>
-                @endif
-                @if (Route::has('register'))
-                <!-- <p class="mb-1">
-                    <a class="text-center" href="{{ route('register') }}">{{ __('Register a new memebership') }}</a>
-                </p> -->
-                @endif
-                
-                    
-                
-    
-                
+<div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+  <div class="card card0 border-0">
+    <div class="row d-flex">
+      <div class="col-xl-6">
+        <div class="card1 pb-5">
+          <div class="row"> <img src="{{ asset('admin/img/kwabey-logo.png') }}" class="logo img-fluid"> </div>
+          <div class="row px-3 justify-content-center mt-4  border-line"> <img src="{{ asset('admin/img/delivery.png') }}" class="image img-fluid "> </div>
+        </div>
+      </div>
+      <div class="col-xl-6 pb-4">
+        <div class="card2 card border-0 mrT">
+          <div class="row mb-4 px-3">
+            <h1 class="mb-0 mr-4 mt-2 welcometxt">Welcome To {{ config('app.name', 'Kwabey') }} Login</h1>
+          </div>
+          <form method="POST" action="{{ route('login') }}">
+            @csrf
+              <div class="row px-3 input-icons">
+                <label class="mb-1">
+                <h6 class="mb-0 text-sm">Email Address</h6>
+                </label>
+                <i class="fa fa-user icon"></i>
+                <input id="email" type="email" placeholder="Enter a valid email address" class="mb-4 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+              <div class="row px-3 input-icons">
+                <label class="mb-1">
+                <h6 class="mb-0 text-sm">Password</h6>
+                </label>
+                <i class="fa fa-lock icon"></i>
+                <input id="password" placeholder="Enter password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+              <div class="row px-3 mb-4">
+                <div class="custom-control custom-checkbox custom-control-inline">
+                  <input type="checkbox" id="remember" name="remember" class="custom-control-input" {{ old('remember') ? 'checked' : '' }} >  
+                  <label for="chk1" class="custom-control-label text-sm">Remember me</label>
                 </div>
-                <!-- /.login-card-body -->
-            </div>
+                @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}" class="ml-auto mb-0 text-sm">Forgot Password?</a> 
+                @endif
+              </div>
+              <div class="row mb-3 px-3">
+                <button type="submit" class="btn log-in btn-success text-center">Login</button>
+              </div>
+            </form>
         </div>
-        </div>
+      </div>
     </div>
+  </div>
 </div>
 <script type="text/javascript">
 localStorage.removeItem('isModalVisible');
